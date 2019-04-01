5c2fc03b483bdcad97634c7f7332706f0f4a27ac
maniacata <maniaciachao@gmail.com>
2019-04-01 18:17:12 +0800

    replace ycy with yzy

--- src/reminderView.ts
+++ src/reminderView.ts
@@ -1,38 +1,46 @@
-'use strict';
-import * as vscode from 'vscode';
-import * as path from 'path';
+"use strict";
+import * as vscode from "vscode";
+import * as path from "path";
 
 export class ReminderView {
-    private static panel: vscode.WebviewPanel | undefined;
+  private static panel: vscode.WebviewPanel | undefined;
 
-    public static show(context: vscode.ExtensionContext) {
-        if (this.panel) {
-            this.panel.reveal();
-        } else {
-            this.panel = vscode.window.createWebviewPanel("ycy", "杨超越", vscode.ViewColumn.Two, {
-                enableScripts: true,
-                retainContextWhenHidden: true,
-            });
+  public static show(context: vscode.ExtensionContext) {
+    if (this.panel) {
+      this.panel.reveal();
+    } else {
+      this.panel = vscode.window.createWebviewPanel(
+        "yzy",
+        "杨子越",
+        vscode.ViewColumn.Two,
+        {
+          enableScripts: true,
+          retainContextWhenHidden: true
+        }
+      );
 
-            const imagePath = vscode.Uri.file(path.join(context.extensionPath, 'images', 'ycy0.png'))
-                .with({ scheme: 'vscode-resource' });
+      const imagePath = vscode.Uri.file(
+        path.join(context.extensionPath, "images", "yzy0.jpg")
+      ).with({ scheme: "vscode-resource" });
 
-            this.panel.webview.html = `<!DOCTYPE html>
+      this.panel.webview.html = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>杨超越</title>
+    <title>杨子越</title>
 </head>
-<body>
-    <div><h1>小哥哥~ 代码写久了，该休息啦~</h1></div>
-    <div><img src="${imagePath}"></div>
+<body style="background:#dc2424;">
+    <div style="text-align:center;">
+        <div><h1>歇会儿吧大佬，你是不可能写完的</h1></div>
+        <div><img src="${imagePath}"></div>
+    </div>
 </body>
 </html>`;
 
-            this.panel.onDidDispose(() => {
-                this.panel = undefined;
-            });
-        }
+      this.panel.onDidDispose(() => {
+        this.panel = undefined;
+      });
     }
+  }
 }
